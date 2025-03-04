8ab1517b6efc3112ee3d3e0d0da72010cb978681
ThePowerfulProgrammer <ashishr0301@gmail.com>
2025-03-04 15:41:28 +0200

    loaded in a texture and using it on a cube

--- discoverThreeJS/components/cube.js
+++ discoverThreeJS/components/cube.js
@@ -2,21 +2,31 @@ import {BoxBufferGeometry, Mesh, MeshBasicMaterial,
      BufferGeometry,
     BufferAttribute,
     MeshStandardMaterial,
-    MathUtils
+    MathUtils,
+    TextureLoader,
 } from "https://cdn.skypack.dev/three@0.132.2";
 
 
+
 const radiansPerSecond = MathUtils.degToRad(30);
+
+function createMaterial() 
+{
+    const textureLoader = new TextureLoader();
+    const texture = textureLoader.load("../assets/textures/uv-test-bw.png");
+
+    const material = new MeshStandardMaterial({map: texture});
+    return material;
+}
+
 // The geometry defines the shape of the mesh. 
 // While the geometry defines the shape, the material defines how the surface of the mesh looks.BoxBufferGeometry
 // Meshes are the most common kind of visible object used in 3D computer graphics, 
 // and are used to display all kinds of 3D objects
-
-
 function createCube() 
 {
     const geometry = new BoxBufferGeometry(2,2,2);
-    const material = new MeshStandardMaterial({color: "purple" } );
+    const material = createMaterial();
     const cube = new Mesh(geometry, material)
     cube.position.set(-2,0,0);
     cube.rotation.set(-0.5,-0.1,0.8); // let us actually rotate our cube so it looks like a cube 
