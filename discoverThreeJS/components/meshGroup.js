17a359868eaaaef4363b4a117faeb09611ecd581
ThePowerfulProgrammer <ashishr0301@gmail.com>
2025-03-10 13:57:22 +0200

    Changed cube.js to meshGroup.js, the file now contains code showing how to create groups in 3.js

---
+++ discoverThreeJS/components/meshGroup.js
@@ -0,0 +1,48 @@
+import {
+    SphereBufferGeometry,
+    Group,
+    MathUtils,
+    Mesh, 
+    MeshStandardMaterial
+} from "https://cdn.skypack.dev/three@0.132.2";
+
+function createMeshGroup() 
+{
+    const group = new Group();
+
+    const geometry = new SphereBufferGeometry(0.25, 16,16);
+
+    const material = new MeshStandardMaterial({
+        color: "indigo",
+    })
+
+    const protoSphere = new Mesh(geometry, material);
+
+    group.add(protoSphere);
+
+    // using 1 and 0.05 instead of 1 and 20 because we want to arrange the spheres in a circle
+    for (let i=0;i<1;i+= 0.05) 
+        {
+            const sphere = protoSphere.clone();
+
+            sphere.position.x = Math.cos(2 * Math.PI * i);
+            sphere.position.y = Math.sin(2 * Math.PI * i);
+
+            sphere.scale.multiplyScalar(0.01 + i);
+            group.add(sphere);
+        }
+
+    
+    group.scale.multiplyScalar(2);
+
+    const radiansPerSecond = MathUtils.degToRad(30);
+
+    group.tick = (delta) => 
+        {
+            group.rotation.z -= delta * radiansPerSecond;
+
+        };
+    return group;
+}
+
+export {createMeshGroup};
